7e7850c650378da90f96fabe40d57e4c66fbca0c
Esar Behlum <esarnb@gmail.com>
2019-05-20 18:22:26 -0700

    Initial html, css, game.js files

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+/**
+ * Word Search Game
+ * 
+ * By: Esar Behlum
+ */
+
+//  List of potential words to use
+
+var potentialWords = [];
+
+// Wins variable //array
+
+var winCount = 0;
+
+// Number of guesses remaining //variable
+
+var guessCount = 12;
+
+// Letters of keys guessed [User hould not be punished twice] //array
+
+var usedKeys = [];
+
+// Current Word:  _ _ _ _ _ _ //changing string
+/* Word Logic: */
+
